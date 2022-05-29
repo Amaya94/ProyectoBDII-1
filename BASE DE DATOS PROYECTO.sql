82913677100b11093bca7e0bf5cbda6f18e6b086
Josue Amaya <jamayao@miumg.edu.gt>
2022-05-29 17:57:33 -0600

    stored Procedure
    Modificaciones al Stored Procedure

--- BASE DE DATOS PROYECTO.sql
+++ BASE DE DATOS PROYECTO.sql
@@ -58,70 +58,76 @@ INSERT INTO Pagos VALUES ('98','CASTILLO ARMAS','3200','250','0','0','0','0','0'
 INSERT INTO Pagos VALUES ('235','PALACIOS ORDOÑEZ','3200','250','0','0','0','0','0');
 
 SELECT * FROM Pagos;
+USE `proyectofinal_2`;
+DROP procedure IF EXISTS `calc_comisiones`;
 
-/*
-CREATE DEFINER=`root`@`localhost` PROCEDURE `comisiones_venta`
-(in Area char, in Ventas_Actuales int, in Meta int)
+DELIMITER $$
+USE `proyectofinal_2`$$
+CREATE PROCEDURE `calc_comisiones` ()
 BEGIN
-/*
-   DECLARE IRTRA float;
-   DECLARE ISR float;
-   DECLARE IGSS float;
-   DECLARE comision int;
-   DECLARE total float;
-   DECLARE tot_desc float;
+	DECLARE Tot_Desc FLOAT;
+    DECLARE Comision INT;
+    DECLARE IGSS, IRTRA, ISR, Liquido_A_Recibir FLOAT;
+    
+	SELECT Apellidos_Supervisor, Sueldo, Bonificacion, codigo_ventas FROM Ventas;
+    SELECT Area, Estatus_Supervisores, codigo_Supervisores FROM Supervisores;
+    SELECT Ventas_Actuales, Metas_No_Alcanzadas FROM Ventas;
+    
+    IF @Area = 'JARDINERIA' THEN
+		IF @Ventas_Actuales >= 1000 AND @Ventas_Actuales <3000 THEN
+			SET Comision = 100;
+		END IF;
+		IF @Ventas_Actuales >= 3001 AND @Ventas_Actuales <5000 THEN
+			SET Comision = 300;
+		END IF;
+		IF @Ventas_Actuales >= 5001 AND @Ventas_Actuales <8000 THEN
+			SET Comision = 500;
+		END IF;
+        IF @Ventas_Actuales >= 8001 AND @Ventas_Actuales <10000 THEN
+			SET Comision = 700;
+		END IF;
+        IF @Ventas_Actuales >= 10001 THEN
+			SET Comision = 1000;
+		END IF;
+		SET IGSS = Sueldo * 0.0487;
+        SET IRTRA = Sueldo * 0.01;
+        SET ISR = Sueldo * 0.01;
+        SET Tot_Desc = IGSS+IRTRA+ISR;
+        
+        IF @Metas_No_Alcanzadas = 0 THEN
+			SET Liquido_A_Recibir = (Sueldo_Base - Tot_Desc) + Comision + Bonificacion;
+		END IF;
+        IF @Metas_No_Alcanzadas = 1 THEN
+			SET Liquido_A_Recibir = (Sueldo_Base - Tot_Desc) + Bonificacion;
+		END IF;
+        IF @Metas_No_Alcanzadas = 2 THEN
+			SET Liquido_A_Recibir = 0;
+		END IF;
+        IF @Metas_No_Alcanzadas >= 3 THEN
+			SET Liquido_A_Recibir = 0;
+		END IF;
 
-	
-   IF Area = "jardineria" THEN
-      SELECT codigo_ventas FROM Ventas WHERE codigo_ventas=codigo_Supervisores INTO Supervisores;
-      
-	IF Ventas_Actuales < 1000 THEN
-      SET comision = 0;
-    END IF;  
-	IF Ventas_Actuales >1000 && Ventas_Actuales <= 3000 THEN
-      SET comision = 100;
-    END IF;  
-	IF Ventas_Actuales >3001 && Ventas_Actuales <= 5000 THEN
-      SET comision = 300;
-    END IF;  
-	IF Ventas_Actuales >5001 && Ventas_Actuales <= 8000 THEN
-      SET comision = 500;
-    END IF;  
-	IF Ventas_Actuales >8001 && Ventas_Actuales <= 10000 THEN
-      SET comision = 700;
-    END IF;  
-    IF Ventas_Actuales >10001  THEN
-      SET comision = 1000;
-    END IF;
-    
-	SET IGSS =  Sueldo_Base  * 0.0487;
-    SET ISR = Sueldo_Base * 0.05;
-    SET IRTRA = Sueldo_Base * 0.01;
-    SET tot_desc = IGSS + ISR + IRTRA;
-    
-    IF Metas_No_Alcanzadas = 0 THEN
-	 SET total = (Sueldo_Base - tot_desc) + comision + Bonificacion;
-    END IF;
-    IF Metas_No_Alcanzadas = 1 THEN
-	 SET total = (Sueldo_Base - tot_desc)  + Bonificacion;
-    END IF; 
-    IF Metas_No_Alcanzadas = 2 THEN
-	 SET total = 0;
-	END IF;
-    IF Metas_No_Alcanzadas >= 3 THEN
-	 SET total = 0;
-	END IF;
-     
-	UPDATE Supervisores SET Estatus_Supervisores = "BAJA" ;
-	INSERT INTO Pagos VALUES (codigo_ventas,Apellidos_Supervisor,Sueldo,Bonificacion,comision,IGSS,ISR,IRTRA,total);
-   END IF;
-   
-   /*SET IVA=subtotal*0.12;
-   SET Total=subtotal+IVA;
-   INSERT INTO factura VALUES (numfact, numpedido, cliente, subtotal, IVA, total);
-   UPDATE pedidos set Status=1 WHERE pedido_id=numpedido;
-   SELECT * FROM factura;
-   SELECT * FROM pedidos;*/
-/*
-END
-*/
+		IF @Metas_No_Alcanzadas >= 3 THEN
+			UPDATE Supervisores
+            SET Estatus_Supervisores = 'BAJA'
+            WHERE codigo_Supervisores = codigo_pagos ;
+		END IF;
+        
+        IF @codigo_pagos = @codigo_Supervisores THEN
+			UPDATE Pagos
+			SET Comision = @comision;
+			SET IGSS = @IGSS;
+			SET ISR = @ISR;
+			SET IRTRA = @IRTRA;
+			SET Liquido_A_Recibir = @Liquido_A_Recibir;
+			/*WHERE codigo_Pagos = codigo_Supervisores;*/
+		END IF;
+        END IF;
+END$$
+
+DELIMITER ;
+
+CALL calc_comisiones;
+SELECT * FROM Pagos;
+SELECT * FROM Ventas;
+SELECT * FROM Supervisores;
